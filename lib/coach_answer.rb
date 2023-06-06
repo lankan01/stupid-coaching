e36aca6bfdb509d6f963455e3c9e93dc2360ca58
Jega Pradeeba <pjega92@gmail.com>
2023-06-06 14:00:05 +0800

    trial 20

--- lib/coach_answer.rb
+++ lib/coach_answer.rb
@@ -1,26 +1,21 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_messagesss
-  if your_message == "I am going to work right now SIR !" then
-    return ""
-  elsif your_message.include?("?") then
-    return "Silly question, get dressed and go to work !"
-  else
-    return "I don't care son, get dressed and go to work !"
-  end
+  # if your_message == "I am going to work right now SIR !" then
+  #   return ""
+  # elsif your_message.include?("?") then
+  #   return "Silly question, get dressed and go to work !"
+  # else
+  #   return "I don't care son, get dressed and go to work !"
+  # end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
-  if your_message == "I am going to work right now SIR !" then
-    return ""
-  elsif your_message.upcase == your_message then
-    return "I can feel your motivation ! #{coach_answer(your_message)}"
-  else
-    return coach_answer(your_message)
-    #commennntt
-    #commennntt
-    #commennntt
-    #commennntt
-    #commennntt
-  end
+  # if your_message == "I am going to work right now SIR !" then
+  #   return ""
+  # elsif your_message.upcase == your_message then
+  #   return "I can feel your motivation ! #{coach_answer(your_message)}"
+  # else
+  #   return coach_answer(your_message)
+  # end
 end
