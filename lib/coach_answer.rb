04ffb1ef096b8d59df34020f1666a05fdb826cc2
Jega Pradeeba <pjega92@gmail.com>
2023-06-06 14:21:20 +0800

    commit

--- lib/coach_answer.rb
+++ lib/coach_answer.rb
@@ -1,12 +1,12 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_messagesss
-  if your_message == "I am going to work right now SIR !" then
-    return ""
-  elsif your_message.include?("?") then
-    return "Silly question, get dressed and go to work !"
-  else
-    return "I don't care son, get dressed and go to work !"
-  end
+  # if your_message == "I am going to work right now SIR !" then
+  #   return ""
+  # elsif your_message.include?("?") then
+  #   return "Silly question, get dressed and go to work !"
+  # else
+  #   return "I don't care son, get dressed and go to work !"
+  # end
 end
 
 def coach_answer_enhanced(your_message)
